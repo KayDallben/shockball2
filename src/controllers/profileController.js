d9be342e5731440717256e4581e067bf713b4b77
bpkennedy <bpkennedy@gmail.com>
2018-02-03 20:38:06 -0600

    figured out the issue, yay

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -15,11 +15,8 @@ class ProfileController {
   }
 
   async listOne(req, res) {
-    console.log('got in the listOne controller') //eslint-disable-line no-console
     const validation = Joi.validate(req.query, ProfileSchema.listOneParams)
     if (validation.error === null) {
-      console.log('req.uid is actually%%%%%%%%%%%%%%%%%%%%%%') //eslint-disable-line no-console
-      console.log(req.uid) //eslint-disable-line no-console
       try {
         const player = await this.checkIfPlayerExists(req.uid)
         if (player) {
@@ -63,29 +60,18 @@ class ProfileController {
   }
 
   checkIfPlayerExists(uid) {
-    console.log('uid in checkIfPlayerExists shows as __________________________') //eslint-disable-line no-console
-    console.log(uid) //eslint-disable-line no-console
     return this.players.doc(uid).get().then(doc => {
-      console.log('firebase player check response is (((((((((((((((((((((') //eslint-disable-line no-console
-      console.log(doc) //eslint-disable-line no-console
       if (doc.exists) {
-        console.log('doc exists so trying to return data @@@@@@@@@@@@@@@@@@@@@@@@@') //eslint-disable-line no-console
-        console.log(doc.data()) //eslint-disable-line no-console
         return doc.data()
       } else {
-        console.log('doc does not exist, returning false ^^^^^^^^^^^^^^^^^^^^^^^^^^') //eslint-disable-line no-console
         return false
       }
     }).catch(error => {
-      console.log('there was an error calling firebase, $$$$$$$$$$$$$$$$$$$$$$') //eslint-disable-line no-console
-      console.log(error) //eslint-disable-line no-console
       this.logger.error(error)
     })
   }
 
   getCharacterInfo(uid, accessToken) {
-    console.log('uid is========================== ') //eslint-disable-line no-console
-    console.log(uid) //eslint-disable-line no-console
     return axios({
       url: `http://www.swcombine.com/ws/v1.0/character/${uid}`,
       method: 'GET',
@@ -96,8 +82,6 @@ class ProfileController {
         'Accept': 'application/json'
       }
     }).then(response => {
-      console.log('response from swc /character call is==================') //eslint-disable-line no-console
-      console.log(response) //eslint-disable-line no-console
       return response.data
     }).catch(error => {
       this.logger.error('Error calling character info at SWC')
