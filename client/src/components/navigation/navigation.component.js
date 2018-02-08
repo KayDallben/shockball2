63f5fa9d4b69ab8136c9efb2a2a0385804efbe62
bpkennedy <bpkennedy@gmail.com>
2018-02-07 21:22:39 -0600

    adding more nav styles

--- client/src/components/navigation/navigation.component.js
+++ client/src/components/navigation/navigation.component.js
@@ -1,6 +1,26 @@
 export const navigationComponent = {
   template: `
     <div class="nav-wrapper">
+      <div>
+        <div class="title-icon fa fa-user"></div>
+        <div class="title">Me</div>
+      </div>
+      <div>
+        <div class="title-icon fa fa-users"></div>
+        <div class="title">Squad</div>
+      </div>
+      <div>
+        <div class="title-icon fa fa-briefcase"></div>
+        <div class="title">Office</div>
+      </div>
+      <div>
+        <div class="title-icon fa fa-flag"></div>
+        <div class="title">League</div>
+      </div>
+      <div>
+        <div class="title-icon fa fa-building"></div>
+        <div class="title">Transfers</div>
+      </div>
     </div>
   `,
   bindings: {
