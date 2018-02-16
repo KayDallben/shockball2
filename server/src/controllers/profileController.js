1154e639d4baf3f7fe8c82a1d87bc9189c3a35c2
bpkennedy <bpkennedy@gmail.com>
2018-02-16 11:45:57 -0600

    adding skills generation

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -2,15 +2,19 @@
 import Joi from 'joi'
 import axios from 'axios'
 import * as admin from 'firebase-admin'
-const FieldValue = admin.firestore.FieldValue
+import Chance from 'chance'
 
 //internal
 import ProfileSchema from '../models/Profile.js'
+
+const FieldValue = admin.firestore.FieldValue
+const chance = new Chance()
 
 class ProfileController {
 
   constructor(db, logger) {
     this.players = db.collection('players')
+    this.playerCaps = db.collection('playerCaps')
     this.logger = logger
   }
 
@@ -42,6 +46,8 @@ class ProfileController {
 
   async createNewPlayer(uid, accessToken) {
     try {
+      const baseStats = this.rollBaseStats()
+      await this.createPlayerStatCaps(uid)
       const swcCharacter = await this.getCharacterInfo(uid, accessToken)
       await this.players.doc(uid).set({
         name: swcCharacter.character.name,
@@ -49,7 +55,17 @@ class ProfileController {
         gender: swcCharacter.character.gender,
         race: swcCharacter.character.race.value,
         created: FieldValue.serverTimestamp(),
-        createdAsUid: uid
+        createdAsUid: uid,
+        passing: baseStats.passing,
+        throwing: baseStats.throwing,
+        blocking: baseStats.blocking,
+        toughness: baseStats.toughness,
+        morale: baseStats.morale,
+        vision: baseStats.vision,
+        leadership: baseStats.leadership,
+        aggression: baseStats.aggression,
+        endurance: baseStats.endurance,
+        fatigue: baseStats.fatigue
       })
       return await this.players.doc(uid).get().then(doc => {
         return doc.data()
@@ -58,6 +74,36 @@ class ProfileController {
       this.logger.error(error)
       return false
     }
+  }
+
+  rollBaseStats() {
+    const baseStats = {
+      passing: chance.integer({min:40, max:60}),
+      throwing: chance.integer({min:40, max:60}),
+      blocking: chance.integer({min:40, max:60}),
+      toughness: chance.integer({min:40, max:60}),
+      endurance: chance.integer({min:40, max:60}),
+      vision: chance.integer({min:40, max:60}),
+      morale: chance.integer({min:70, max:100}),
+      leadership: chance.integer({min:10, max:80}),
+      aggression: 0,
+      fatigue: 0
+    }
+    return baseStats
+  }
+
+  async createPlayerStatCaps(uid) {
+    const playerWithStatCaps = {
+      createdAsUid: uid,
+      created: FieldValue.serverTimestamp(),
+      passing: chance.integer({min:75, max:100}),
+      throwing: chance.integer({min:75, max:100}),
+      blocking: chance.integer({min:75, max:100}),
+      toughness: chance.integer({min:75, max:100}),
+      endurance: chance.integer({min:75, max:100}),
+      vision: chance.integer({min:75, max:100})
+    }
+    return await this.playerCaps.doc(uid).set(playerWithStatCaps)
   }
 
   checkIfPlayerExists(uid) {
